e0b0f7f37227943c3e8975aba5db960e89ff768f
Julio <valenteonion@gmail.com>
2024-10-29 18:34:26 -0300

    teste

---
+++ aula1/pratica1/olamundo.c
@@ -0,0 +1,16 @@
+//Essa foi a primeira aula, estou fazendo de novo pq perdi a conta no github. Farei o mesmo com as outras aulas
+
+// esse é o ''esqueleto'' de toda programação em C.
+
+#include <stdio.h>
+
+int main(){
+
+printf("Olá mundo!");
+
+
+
+
+
+    return 0;
+}
