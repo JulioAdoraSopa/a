21b31257442476ac1512646c39465d26e871d8a1
JulioAdoraSopa <valenteonion@gmail.com>
2024-10-30 00:19:56 -0300

    aula6

--- aula6/pratica6/estrutura_if.c
+++ aula6/pratica6/estrutura_if.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
 
+int n1, n2, result;
+
+char sinal;
 
 
+printf("Digite dois valores");
+scanf("%i", &n1);
+scanf("%i", &n2);
+printf("digite + para somar ou - para subtrair:\n");
+scanf("%c", &sinal);
+
+switch (sinal) {
+case '+':
+
+    result = n1 + n2;
+    printf("A soma e: %i\n", result);
+    break; 
+case '-':
+    result = n1 - n2;
+    printf("A subtracao e: %i\n", result);
+    break;
+    default:
+    printf("operacao invalida");
+
+}
 
 
 
