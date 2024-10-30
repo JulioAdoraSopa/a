f0351006ecfcd598ca10598b027e4fdddde20f0a
JulioAdoraSopa <valenteonion@gmail.com>
2024-10-30 10:10:40 -0300

    strings e matrizes

--- aulastrings/strings.c
+++ aulastrings/strings.c
@@ -3,6 +3,21 @@
 int main (){
 
 
+int numeros 10;// array ou vetor
+
+numeros [0] = 10 //primeira posicão
+numeros [1] = 20 //segunda posicão
+numeros [2] = 30 //terceira posicão
+numeros [3] = 40 //quarta posicão
+numeros [4] = 50 //quinta ''
+numeros [5] = 60 // sexta ''
+numeros [6] = 70 //sétima ''
+numeros [7] = 80 //oitava ''
+numeros [8] = 90 // nona ''
+
+//MATRIZ
+
+
 
 
 
