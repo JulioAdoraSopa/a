c6b64466174650bbda60369ee484638d57856ea1
JulioAdoraSopa <valenteonion@gmail.com>
2024-11-06 10:04:22 -0300

    localizac

---
+++ pratica8/localiza.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+ int main(){
+
+    int numeros
+    for int (i=0; i < 10; i++)
+    {
+        
+    }
+    
+    scanf(%i);
+
+    int achou 
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+ }
