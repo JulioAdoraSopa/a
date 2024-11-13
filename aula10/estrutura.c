bafa79e4322b521800ab8cdaccdac2026f6cb64c
JulioAdoraSopa <valenteonion@gmail.com>
2024-11-13 10:25:15 -0300

    comentários sobre estruturas

---
+++ aula10/estrutura.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+<string.h>
+int main (){
+
+[struct contato_t
+{
+   
+};
+]
+    /*Comentarios importantes sobre estruturas: [usar isso no trabalho.]
+
+
+    variavel.algo;
+
+    struct serve pra agrupar variaveis;
+
+    Estudar % (percentuais) ----> LEMBRETE
+
+    %s não tem "i comercial"
+    ---------------------------------------------
+
+    struct contatos c matriz;
+
+    type dev (?)
+
+
+    ENUMERAÇÃO:
+
+    enumeração é um conjunto de constantes;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
+
+    return 0;
+}
---
+++ pratica9/nome_completo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+<string.h>
+int main (){
+
+
+//E-3
+
+
+
+    return 0;
+}
