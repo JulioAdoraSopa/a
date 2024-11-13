f7f2a1e2a0deff5664fdd2be322b42121c9573c4
JulioAdoraSopa <valenteonion@gmail.com>
2024-11-13 08:46:40 -0300

    preenche_string

---
+++ pratica9/preenche_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+char str [11];
+
+memset(string, '\0', sizeof(string));
+
+char tecla;
+scanf("%c");
+memset(string, tecla, 10);
+printf("%s\n", string);
+memset(string, '*', 5);
+printf("%s\n", string);
+
+for(int i=0; i<10; i++) {
+
+}
+printf("%c", string[i]);
+printf("\n");
+
+
+
+
+
+
+
+
+    return 0;
+}
